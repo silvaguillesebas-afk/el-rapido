45e57026f0c80a1791ed9498ff3a72fa307dbb9e
silvaguillesebas-afk <silvaguillesebas@gmail.com>
2026-05-22 19:27:23 -0300

    Create config/app.php

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+// Configuración general de la aplicación
+define('APP_NAME', 'El Rápido');
+define('APP_VERSION', '1.0.0');
+define('APP_URL', 'http://localhost/el-rapido');
+define('TIMEZONE', 'America/Argentina/Buenos_Aires');
+
+date_default_timezone_set(TIMEZONE);
+?>
